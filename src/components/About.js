de0574e951c2f53accf2f75d27ddb117315a2133
Gunjan Sachdeva <gunjan.sachdeva@netsmartz.local>
2022-11-19 21:57:20 +0530

    about

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+class About extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h2>My Home Page</h2>
+            </div>
+        )
+    }
+}
+export default About;
